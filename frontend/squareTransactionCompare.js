78d2e2577bcf63f30095f60141c6975c74ee1c28
James Rowan <james.rowan13@gmail.com>
2014-10-05 08:40:41 -0400

    integration with mongoDB, sort of, for square functions

--- frontend/squareTransactionCompare.js
+++ frontend/squareTransactionCompare.js
@@ -6,10 +6,11 @@ var https = require("https");
  * getJSON:  REST get request returning JSON object(s)
  * @param accessToken a valid access token
  * @param lastTimestamp a timestamp of the last time we compared the square database to ours
+ * @param userCollection the db collection containing the database of user info to compare to
  */
 
 
-exports.getJSON = function(accessToken, lastTimestamp)
+exports.getJSON = function(accessToken, lastTimestamp, userCollection)
 {
 	//developed with the help of looking at Bryan Mac's answer on stackexchange here:
 	//http://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
@@ -24,7 +25,7 @@ exports.getJSON = function(accessToken, lastTimestamp)
 		path: '/v1/' + accessToken + '/payments',
 		method: 'GET',
 		headers: {
-			'Authorization': 'Bearer ' + accessToken, //Mg3K9RsvfUduh8m9BSZPmg', //this nneeds to contain the accessToken in general
+			'Authorization': 'Bearer ' + accessToken, //Mg3K9RsvfUduh8m9BSZPmg', //this needs to contain the accessToken in general
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		}
@@ -38,16 +39,29 @@ exports.getJSON = function(accessToken, lastTimestamp)
 	//actually perform the request
 	var r = request.get('https://connect.squareup.com/v1/'+'/payments', function(err, httpResponse, body) {
 		//console.log("made it into the request");
-		var transactionsDetails = JSON.parse(body); //should be a list of Payment objects, we'll extrad ids from them
+		var transactionsDetails = JSON.parse(body); //should be a list of Payment objects, we'll extract ids from them
 		//console.log(transactionsDetails);
+		
+		//we could also in the future when we cover multiple performances include information
+		//about itemization so that we can decrement ticket counter of the right performance
+		
+		//it should look something like this
 		var transactionIDs = [];
 		for(int i = 0; i < transactionsDetails.length; ++i){
 			transactionIDs[i] = transactionsDetails[i].id;
+			//if this transaction isn't in the database yet, enter it
+			if(userCollection.count({ "id" : transactionIDs[i]}) == 0) {
+				userCollection.insert({ "id" : transactionIDs[i]}) //also add other info like which seciton it was once we have that
+			}
 		}
 		//console.log(transactionIDs);
 		
 		//now we do something to the database with these
-		return transactionIDs;
+		
+		//rough mongoDB command sketch
+		
+		//db.usersList.append(
+		//return transactionIDs;
 	});
 
 	// var req = https.request(options, function(res)
